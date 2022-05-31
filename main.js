8a1f48bfbc99a81fa4507a25e7db14a0c308c294
andres alcocer <devandres.tech@gmail.com>
2022-05-31 12:31:53 -0400

    now using state abbreviations

--- main.js
+++ main.js
@@ -3,30 +3,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
   customers.forEach((customer) => {
     const ul = document.createElement('ul')
+    // image
     ul
       .appendChild(document.createElement('li'))
       .appendChild(document.createElement('img')).src = customer.picture.large
+    // full name
     ul
       .appendChild(document.createElement('li'))
       .appendChild(
         document.createElement('h2')
       ).innerHTML = `${customer.name.first} ${customer.name.last}`
+    // email
     ul
       .appendChild(document.createElement('li'))
       .appendChild(document.createElement('p')).innerHTML = customer.email
+    // address
     ul
       .appendChild(document.createElement('li'))
-      .appendChild(
-        document.createElement('p')
-      ).innerHTML = `${customer.location.street.number} 
+      .appendChild(document.createElement('p')).innerHTML = `${
+      customer.location.street.number
+    } 
 		${customer.location.street.name} <br /> 
-		${customer.location.city}, ${customer.location.state}
+		${customer.location.city}, ${nameToAbbr(customer.location.state)}
 		${customer.location.postcode}`
+    // date of birth
     ul
       .appendChild(document.createElement('li'))
       .appendChild(document.createElement('p')).innerHTML = `DOB: ${moment
       .utc(customer.dob.date)
       .format('MMM DD, YYYY')}`
+    // registered date
     ul
       .appendChild(document.createElement('li'))
       .appendChild(
